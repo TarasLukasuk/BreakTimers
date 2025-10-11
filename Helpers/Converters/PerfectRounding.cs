62a5ebe8f6aa64c93bf9bf8a92603971b22fefee
TarasLukasuk <121807502+TarasLukasuk@users.noreply.github.com>
2025-10-11 18:12:41 +0300

    Refactor BreakTimers XAML resources and Timer control: add PerfectRoundingConverter, create RoundedGrid style, merge style and converter dictionaries, and update Timer to use direct bindings for size and alignment

---
+++ Helpers/Converters/PerfectRounding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BreakTimers.Helpers.Converters
+{
+    public class PerfectRoundingConverter : IValueConverter
+    {
+        const int DIVIDE = 2;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not double doubleValue)
+            {
+                throw new ArgumentException($"Value must be a {typeof(double).Name}.");
+            }
+
+            if (doubleValue < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Value must be non-negative.");
+            }
+
+            if (targetType != typeof(CornerRadius))
+            {
+                throw new ArgumentException($"Target type must be {typeof(CornerRadius).Name}.");
+            }
+
+            if (parameter is not null)
+            {
+                throw new ArgumentException("No parameter is expected.");
+            }
+
+            if (culture is not null)
+            {
+                throw new ArgumentException("No culture is expected.");
+            }
+
+
+            return new CornerRadius(doubleValue / DIVIDE);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
